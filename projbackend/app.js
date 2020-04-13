54e4bb0491034f5737c07e0d2fa26065ea0a8de2
rupesh1310 <a.rupesh66@gmail.com>
2020-04-14 02:35:36 +0530

    :beers: common middlewares

--- projbackend/app.js
+++ projbackend/app.js
@@ -3,6 +3,9 @@ require("dotenv").config();
 const mongoose = require("mongoose");
 const express = require("express");
 const app = express();
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
 
 mongoose
   .connect(process.env.DATABASE, {
@@ -14,7 +17,11 @@ mongoose
     console.log("DB CONNECTED");
   });
 
-const port = 8000;
+app.use(bodyParser.json());
+app.use(cookieParser());
+app.use(cors());
+
+const port = process.env.PORT || 8000;
 
 app.listen(port, () => {
   console.log(`app is running at ${port}`);
--- testback/index.js
+++ testback/index.js
@@ -3,9 +3,26 @@ const express = require("express");
 const app = express();
 
 const port = 8000;
+
 app.get("/", (req, res) => {
-  return res.send("Homepage");
+  return res.send("Home page");
 });
+
+const admin = (req, res) => {
+  return res.send("this is admin dashboard");
+};
+
+const isAdmin = (req, res, next) => {
+  console.log("isAdmin is running");
+  next();
+};
+
+const isloggedIn = (req, res, next) => {
+  console.log("Logged in");
+  next();
+};
+
+app.get("/admin", isAdmin, isloggedIn, admin);
 
 app.get("/login", (req, res) => {
   return res.send("You are visiting login route");
