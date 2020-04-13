a4ccc4aeb1cfbdd8d0df6783e6cbbec45086252f
rupesh1310 <a.rupesh66@gmail.com>
2020-04-14 04:33:21 +0530

    :building_construction: validation in routes is setuP

--- projbackend/routes/auth.js
+++ projbackend/routes/auth.js
@@ -1,8 +1,17 @@
 var express = require("express");
 var router = express.Router();
+const { check, validationResult } = require("express-validator");
 const { signout, signup } = require("../controllers/auth");
 
-router.post("/signup", signup);
+router.post(
+  "/signup",
+  [
+    check("name", "name should be atleast 3 char").isLength({ min: 5 }),
+    check("email", "email is required").isEmail(),
+    check("password", "password should be atleast 3 char").isLength({ min: 3 }),
+  ],
+  signup
+);
 router.get("/signout", signout);
 
 module.exports = router;
