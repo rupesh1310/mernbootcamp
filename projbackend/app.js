4e1538ea602cb1bb0c70275cb29da749cc5a40c6
rupesh1310 <a.rupesh66@gmail.com>
2020-04-14 02:54:25 +0530

    :beers: adding routes

--- projbackend/app.js
+++ projbackend/app.js
@@ -7,6 +7,9 @@ const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
 const cors = require("cors");
 
+const authRoutes = require("./routes/auth");
+
+// DB connection
 mongoose
   .connect(process.env.DATABASE, {
     useNewUrlParser: true,
@@ -17,12 +20,18 @@ mongoose
     console.log("DB CONNECTED");
   });
 
+// This is my middlewares
 app.use(bodyParser.json());
 app.use(cookieParser());
 app.use(cors());
 
+// My Routes
+app.use("/api", authRoutes);
+
+// PORT
 const port = process.env.PORT || 8000;
 
+//Starting a server
 app.listen(port, () => {
   console.log(`app is running at ${port}`);
 });
