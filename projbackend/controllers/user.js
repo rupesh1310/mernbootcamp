7b6cee125fe0a3a28bd857911bb21d4416ae3f8a
rupesh1310 <a.rupesh66@gmail.com>
2020-04-14 07:46:48 +0530

    :beetle: fixed a major bug

--- projbackend/controllers/user.js
+++ projbackend/controllers/user.js
@@ -13,6 +13,9 @@ exports.getUserById = (req, res, next, id) => {
 };
 
 exports.getUser = (req, res) => {
-  //TODO: get back here for password
+  req.profile.salt = undefined;
+  req.profile.encry_password = undefined;
+  req.profile.createdAt = undefined;
+  req.profile.updatedAt = undefined;
   return res.json(req.profile);
 };
