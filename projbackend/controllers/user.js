c025f583656cad78192fad90eadd6dae843a9e45
rupesh1310 <a.rupesh66@gmail.com>
2020-04-14 09:06:53 +0530

    :building_construction: :zap: one more middleware to update purchases

--- projbackend/controllers/user.js
+++ projbackend/controllers/user.js
@@ -51,3 +51,33 @@ exports.userPurchaseList = (req, res) => {
       return res.json(order);
     });
 };
+
+exports.pushOrderInPurchaseList = (req, res, next) => {
+  let purchases = [];
+  req.body.order.products.forEach((product) => {
+    purchases.push({
+      _id: product._id,
+      name: product.name,
+      description: product.description,
+      category: product.category,
+      quantity: product.quantity,
+      amount: req.body.order.amount,
+      transaction_id: req.body.order.transaction_id,
+    });
+  });
+
+  // Store this in DB
+  User.findOneAndUpdate(
+    { _id: req.profile._id },
+    { $push: { purchases: purchases } },
+    { new: true },
+    (err, purchases) => {
+      if (err) {
+        return res.status(400).json({
+          error: "Unable to save purchase list",
+        });
+      }
+      next();
+    }
+  );
+};
