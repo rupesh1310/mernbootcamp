44a9990c29df36a4e03d682c3aa6cddc344c1c54
rupesh1310 <a.rupesh66@gmail.com>
2020-04-15 03:28:32 +0530

    :bento: creating category and saving it

--- projbackend/app.js
+++ projbackend/app.js
@@ -1,6 +1,5 @@
 require("dotenv").config();
 
-
 const mongoose = require("mongoose");
 const express = require("express");
 const app = express();
--- projbackend/routes/category.js
+++ projbackend/routes/category.js
@@ -1,11 +1,21 @@
 const express = require("express");
 const router = express.Router();
 
-const { getCategoryById } = require("../controllers/category");
+const { getCategoryById, createCategory } = require("../controllers/category");
 const { isSignedIn, isAdmin, isAuthenticated } = require("../controllers/auth");
 const { getUserById } = require("../controllers/user");
 
+//params
 router.param("userId", getUserById);
 router.param("categoryId", getCategoryById);
+
+//actual routers goes here
+router.post(
+  "/category/create/:userId",
+  isSignedIn,
+  isAuthenticated,
+  isAdmin,
+  createCategory
+);
 
 module.exports = router;
