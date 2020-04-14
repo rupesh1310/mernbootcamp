14fc7a3beaeed843909128fd88e673999fc051cd
rupesh1310 <a.rupesh66@gmail.com>
2020-04-14 08:22:52 +0530

    :beers: update user info

--- projbackend/controllers/user.js
+++ projbackend/controllers/user.js
@@ -20,13 +20,20 @@ exports.getUser = (req, res) => {
   return res.json(req.profile);
 };
 
-exports.getAllUsers = (req, res) => {
-  User.find().exec((err, users) => {
-    if (err || !users) {
-      return res.status(400).json({
-        error: "No user was found in DB",
-      });
+exports.updateUser = (req, res) => {
+  User.findByIdAndUpdate(
+    { _id: req.profile._id },
+    { $set: req.body },
+    { new: true, useFindAndModify: false },
+    (err, user) => {
+      if (err) {
+        return res.status(400).json({
+          error: "You are not authorized to update this user",
+        });
+      }
+      user.salt = undefined;
+      user.encry_password = undefined;
+      res.json(user);
     }
-    res.json(users);
-  });
+  );
 };
--- projbackend/routes/user.js
+++ projbackend/routes/user.js
@@ -1,12 +1,12 @@
 const express = require("express");
 const router = express.Router();
 
-const { getUserById, getUser, getAllUsers } = require("../controllers/user");
+const { getUserById, getUser, updateUser } = require("../controllers/user");
 const { isSignedIn, isAuthenticated, isAdmin } = require("../controllers/auth");
 
 router.param("userId", getUserById);
 
 router.get("/user/:userId", isSignedIn, isAuthenticated, getUser);
-router.get("/users", getAllUsers);
+router.put("/user/:userId", isSignedIn, isAuthenticated, updateUser);
 
 module.exports = router;
