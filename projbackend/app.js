d258bc370c8291ef9826b530474c2e7339250546
rupesh1310 <a.rupesh66@gmail.com>
2020-04-14 09:17:28 +0530

    :boom: :bento: further collections

--- projbackend/app.js
+++ projbackend/app.js
@@ -10,6 +10,7 @@ const cors = require("cors");
 //My Routes
 const authRoutes = require("./routes/auth");
 const userRoutes = require("./routes/user");
+const categoryRoutes = require("./routes/category");
 
 // DB connection
 mongoose
@@ -30,6 +31,7 @@ app.use(cors());
 // My Routes
 app.use("/api", authRoutes);
 app.use("/api", userRoutes);
+app.use("/api", categoryRoutes);
 
 // PORT
 const port = process.env.PORT || 8000;
