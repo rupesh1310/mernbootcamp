b433b06f84d97cbb0af29936d8f4275357e3b51a
rupesh1310 <a.rupesh66@gmail.com>
2020-04-15 07:22:48 +0530

    :art: :bento: saving photo in mongo and assets v2

--- projbackend/controllers/product.js
+++ projbackend/controllers/product.js
@@ -1,6 +1,7 @@
 const Product = require("../models/product");
 const formidable = require("formidable");
 const _ = require("lodash");
+const fs = require("fs");
 
 exports.getProductById = (req, res, next, id) => {
   Product.findById(id)
@@ -17,5 +18,38 @@ exports.getProductById = (req, res, next, id) => {
 };
 
 exports.createProduct = (req, res) => {
-  //
+  let form = new formidable.IncomingForm();
+  form.keepExtensions = true;
+
+  form.parse(req, (err, fields, file) => {
+    if (err) {
+      return res.status(400).json({
+        error: "Probelm with image",
+      });
+    }
+
+    //TODO: restrictions on field
+    let product = new Product(fields);
+
+    //handle file here
+    if (file.photo) {
+      if (file.photo.size > 3000000) {
+        return res.status(400).json({
+          error: "File size too big!",
+        });
+      }
+      product.photo.data = fs.readFileSync(file.photo.path);
+      product.photo.contentType = file.photo.type;
+    }
+
+    // save to the DB
+    product.save((err, product) => {
+      if (err) {
+        res.status(400).json({
+          error: "Saving tshirt in DB failed!",
+        });
+      }
+      res.json(product);
+    });
+  });
 };
