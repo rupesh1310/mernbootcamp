d5060d782acdb44fdd289bc508c9f2159e7025a9
rupesh1310 <a.rupesh66@gmail.com>
2020-04-15 11:12:40 +0530

    :art: :building_construction: :triangular_flag_on_post: getting order by ID

--- projbackend/routes/order.js
+++ projbackend/routes/order.js
@@ -4,4 +4,15 @@ const { isSignedIn, isAuthenticated, isAdmin } = require("../controllers/auth");
 const { getUserById, pushOrderInPurchaseList } = require("../controllers/user");
 const { updateStock } = require("../controllers/product");
 
-const {} = require("../controllers/order");
+const { getOrderById } = require("../controllers/order");
+
+//params
+router.param("userId", getUserById);
+router.param("orderId", getOrderById);
+
+//Actual routes
+//create
+
+//read
+
+module.exports = router;
