76ade7a2d83001502c93adbf404daeba1eed44dc
rupesh1310 <a.rupesh66@gmail.com>
2020-04-15 07:59:57 +0530

    :ambulance: :triangular_flag_on_post: debugging processed

--- projbackend/app.js
+++ projbackend/app.js
@@ -7,13 +7,13 @@ const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
 const cors = require("cors");
 
-//My Routes
+//My routes
 const authRoutes = require("./routes/auth");
 const userRoutes = require("./routes/user");
 const categoryRoutes = require("./routes/category");
 const productRoutes = require("./routes/product");
 
-// DB connection
+//DB Connection
 mongoose
   .connect(process.env.DATABASE, {
     useNewUrlParser: true,
@@ -24,18 +24,18 @@ mongoose
     console.log("DB CONNECTED");
   });
 
-// This is my middlewares
+//Middlewares
 app.use(bodyParser.json());
 app.use(cookieParser());
 app.use(cors());
 
-// My Routes
+//My Routes
 app.use("/api", authRoutes);
 app.use("/api", userRoutes);
 app.use("/api", categoryRoutes);
 app.use("/api", productRoutes);
 
-// PORT
+//PORT
 const port = process.env.PORT || 8000;
 
 //Starting a server
