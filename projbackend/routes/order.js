1d21cf9a517812ea6d941a1faa55fc831231b3c9
rupesh1310 <a.rupesh66@gmail.com>
2020-04-15 11:21:53 +0530

    :art: :building_construction: :triangular_flag_on_post: creating an order

--- projbackend/routes/order.js
+++ projbackend/routes/order.js
@@ -4,7 +4,7 @@ const { isSignedIn, isAuthenticated, isAdmin } = require("../controllers/auth");
 const { getUserById, pushOrderInPurchaseList } = require("../controllers/user");
 const { updateStock } = require("../controllers/product");
 
-const { getOrderById } = require("../controllers/order");
+const { getOrderById, createOrder } = require("../controllers/order");
 
 //params
 router.param("userId", getUserById);
@@ -12,6 +12,14 @@ router.param("orderId", getOrderById);
 
 //Actual routes
 //create
+router.post(
+  "/order/create/:userId",
+  isSignedIn,
+  isAuthenticated,
+  pushOrderInPurchaseList,
+  updateStock,
+  createOrder
+);
 
 //read
 
