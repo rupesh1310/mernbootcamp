ba021983326080203e4ab209440c0c500de058ae
rupesh1310 <a.rupesh66@gmail.com>
2020-04-15 11:00:29 +0530

    :art: :building_construction: :triangular_flag_on_post: getting started with order

---
+++ projbackend/routes/order.js
@@ -0,0 +1,7 @@
+const express = require("express");
+const router = express.Router();
+const { isSignedIn, isAuthenticated, isAdmin } = require("../controllers/auth");
+const { getUserById, pushOrderInPurchaseList } = require("../controllers/user");
+const { updateStock } = require("../controllers/product");
+
+const {} = require("../controllers/order");
