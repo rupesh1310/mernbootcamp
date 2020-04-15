da4bb64843d8ba1c6a43e10f9adc5dfeee2ad7d7
rupesh1310 <a.rupesh66@gmail.com>
2020-04-15 06:57:57 +0530

    :art: :bento: getting product by ID

---
+++ projbackend/routes/product.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const router = express.Router();
+
+const { getProductById, createProduct } = require("../controllers/product");
+const { isSignedIn, isAuthenticated, isAdmin } = require("../controllers/auth");
+const { getUserById } = require("../controllers/user");
+
+//all of params
+router.param("userId", getUserById);
+router.param("productId", getProductById);
+
+// All of actual routes
+
+module.exports = router;
