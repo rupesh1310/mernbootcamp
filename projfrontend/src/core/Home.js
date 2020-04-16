57c432eaed5a3b11b2bfad6e269bd0eca6c5e078
rupesh1310 <a.rupesh66@gmail.com>
2020-04-17 02:39:05 +0530

    :goal_net: :art: added css in react app

--- projfrontend/src/core/Home.js
+++ projfrontend/src/core/Home.js
@@ -1,9 +1,10 @@
 import React from "react";
+import "../styles.css";
 
 export default function Home() {
   return (
     <div>
-      <h1>Hello front end</h1>
+      <h1 className="text-white">Hello front end</h1>
     </div>
   );
 }
