81caddacb8404991333fc6fb006ced782d01ac78
rupesh1310 <a.rupesh66@gmail.com>
2020-04-17 02:58:11 +0530

    :sparkles: :construction: injecting backend into frontend env.

--- projfrontend/src/core/Home.js
+++ projfrontend/src/core/Home.js
@@ -1,7 +1,10 @@
 import React from "react";
 import "../styles.css";
+import { API } from "../backend";
 
 export default function Home() {
+  console.log("API IS", API);
+
   return (
     <div>
       <h1 className="text-white">Hello front end</h1>
