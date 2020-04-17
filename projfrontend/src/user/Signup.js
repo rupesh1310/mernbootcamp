8162f47553932a857ff84c8e525036796657ef80
rupesh1310 <a.rupesh66@gmail.com>
2020-04-17 06:22:02 +0530

    :lock: :bent: signup component to backend

--- projfrontend/src/user/Signup.js
+++ projfrontend/src/user/Signup.js
@@ -1,8 +1,44 @@
 import React, { useState } from "react";
 import Base from "../core/Base";
 import { Link } from "react-router-dom";
+import { signup } from "../auth/helper";
 
 const Signup = () => {
+  const [values, setValues] = useState({
+    name: "",
+    email: "",
+    password: "",
+    error: "",
+    success: false,
+  });
+
+  const { name, email, password, error, success } = values;
+
+  const handleChange = (name) => (event) => {
+    setValues({ ...values, error: false, [name]: event.target.value });
+  };
+
+  const onSubmit = (event) => {
+    event.preventDefault();
+    setValues({ ...values, error: false });
+    signup({ name, email, password })
+      .then((data) => {
+        if (data.error) {
+          setValues({ ...values, error: data.error, success: false });
+        } else {
+          setValues({
+            ...values,
+            name: "",
+            email: "",
+            password: "",
+            error: "",
+            success: true,
+          });
+        }
+      })
+      .catch(console.log("Error in signup"));
+  };
+
   const signUpForm = () => {
     return (
       <div className="row">
@@ -10,19 +46,32 @@ const Signup = () => {
           <form>
             <div className="form-group">
               <label className="text-light">Name</label>
-              <input className="form-control" type="text" />
+              <input
+                className="form-control"
+                onChange={handleChange("name")}
+                type="text"
+              />
             </div>
-
             <div className="form-group">
               <label className="text-light">Email</label>
-              <input className="form-control" type="email" />
+              <input
+                className="form-control"
+                onChange={handleChange("email")}
+                type="email"
+              />
             </div>
 
             <div className="form-group">
               <label className="text-light">Password</label>
-              <input className="form-control" type="password" />
+              <input
+                onChange={handleChange("password")}
+                className="form-control"
+                type="password"
+              />
             </div>
-            <button className="btn btn-success btn-block">Submit</button>
+            <button onClick={onSubmit} className="btn btn-success btn-block">
+              Submit
+            </button>
           </form>
         </div>
       </div>
@@ -32,6 +81,7 @@ const Signup = () => {
   return (
     <Base title="Sign up page" description="A page for user to sign up!">
       {signUpForm()}
+      <p className="text-light text-center">{JSON.stringify(values)}</p>
     </Base>
   );
 };
