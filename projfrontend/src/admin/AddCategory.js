892a331eb6397045aa2a1134f971dfb725e623af
rupesh1310 <a.rupesh66@gmail.com>
2020-04-18 08:20:18 +0530

    :art: :zap: making category request to backend

--- projfrontend/src/admin/AddCategory.js
+++ projfrontend/src/admin/AddCategory.js
@@ -1,16 +1,49 @@
-import React from "react";
+import React, { useState } from "react";
 import Base from "../core/Base";
+import { isAutheticated } from "../auth/helper";
+import { Link } from "react-router-dom";
+
 const AddCategory = () => {
+  const [name, setName] = useState("initialState");
+  const [error, setError] = useState(false);
+  const [success, setSuccess] = useState(false);
+
+  const { user, token } = isAutheticated();
+
+  const goBack = () => (
+    <div className="mt-5">
+      <Link className="btn btn-sm btn-success mb-3" to="/admin/dashboard">
+        Admin Home
+      </Link>
+    </div>
+  );
+
+  const myCategoryForm = () => (
+    <form>
+      <div className="form-group">
+        <p className="lead">Enter the category</p>
+        <input
+          type="text"
+          className="form-control my-3"
+          autoFocus
+          required
+          placeholder="For Ex. Summer"
+        />
+        <button className="btn btn-outline-info">Create Category</button>
+      </div>
+    </form>
+  );
+
   return (
     <Base
       title="Create a category here"
-      description="Add a new category for new
-      tshirts"
+      description="Add a new category for new tshirts"
       className="container bg-info p-4"
     >
       <div className="row bg-white rounded">
         <div className="col-md-8 offset-md-2">
-          <h1>Hello</h1>
+          {myCategoryForm()}
+          {goBack()}
         </div>
       </div>
     </Base>
