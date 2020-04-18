a59e2784af752bfad1a662db02067d48e97aaacb
rupesh1310 <a.rupesh66@gmail.com>
2020-04-19 05:25:09 +0530

    :art: :zap: added items

--- projfrontend/src/core/Card.js
+++ projfrontend/src/core/Card.js
@@ -1,16 +1,31 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import ImageHelper from "./helper/ImageHelper";
+import { Redirect } from "react-router-dom";
+import { addItemToCart } from "./helper/cartHelper";
 
 const Card = ({ product, addtoCart = true, removeFromCart = false }) => {
+  const [redirect, setRedirect] = useState(false);
+  const [count, setCount] = useState(product.count);
+
   const cartTitle = product ? product.name : "A photo from pexels";
-  const cartDescription = product ? product.description : "Default description";
+  const cartDescrption = product ? product.description : "Default description";
   const cartPrice = product ? product.price : "DEFAULT";
+
+  const addToCart = () => {
+    addItemToCart(product, () => setRedirect(true));
+  };
+
+  const getARedirect = (redirect) => {
+    if (redirect) {
+      return <Redirect to="/cart" />;
+    }
+  };
 
   const showAddToCart = (addtoCart) => {
     return (
       addtoCart && (
         <button
-          onClick={() => {}}
+          onClick={addToCart}
           className="btn btn-block btn-outline-success mt-2 mb-2"
         >
           Add to Cart
@@ -35,9 +50,10 @@ const Card = ({ product, addtoCart = true, removeFromCart = false }) => {
     <div className="card text-white bg-dark border border-info ">
       <div className="card-header lead">{cartTitle}</div>
       <div className="card-body">
+        {getARedirect(redirect)}
         <ImageHelper product={product} />
         <p className="lead bg-success font-weight-normal text-wrap">
-          {cartDescription}
+          {cartDescrption}
         </p>
         <p className="btn btn-success rounded  btn-sm px-4">$ {cartPrice}</p>
         <div className="row">
