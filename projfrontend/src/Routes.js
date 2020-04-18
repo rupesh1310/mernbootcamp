a6622768132bf63e31b5f203d062bb1f2dec16d9
rupesh1310 <a.rupesh66@gmail.com>
2020-04-19 03:59:45 +0530

    :art: :zap: upadting

--- projfrontend/src/Routes.js
+++ projfrontend/src/Routes.js
@@ -11,6 +11,7 @@ import AddCategory from "./admin/AddCategory";
 import ManageCategories from "./admin/ManageCategories";
 import AddProduct from "./admin/AddProduct";
 import ManageProducts from "./admin/ManageProducts";
+import UpdateProduct from "./admin/UpdateProduct";
 
 const Routes = () => {
   return (
@@ -32,7 +33,13 @@ const Routes = () => {
           component={ManageCategories}
         />
         <AdminRoute path="/admin/create/product" exact component={AddProduct} />
+
         <AdminRoute path="/admin/products" exact component={ManageProducts} />
+        <AdminRoute
+          path="/admin/product/update/:productId"
+          exact
+          component={UpdateProduct}
+        />
       </Switch>
     </BrowserRouter>
   );
