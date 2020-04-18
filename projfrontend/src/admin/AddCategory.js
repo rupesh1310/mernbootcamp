f2b2736209716d30d6a07eb70b19ff2152637eba
rupesh1310 <a.rupesh66@gmail.com>
2020-04-18 09:08:47 +0530

    :art: :zap: finishing create categories

--- projfrontend/src/admin/AddCategory.js
+++ projfrontend/src/admin/AddCategory.js
@@ -2,9 +2,10 @@ import React, { useState } from "react";
 import Base from "../core/Base";
 import { isAutheticated } from "../auth/helper";
 import { Link } from "react-router-dom";
+import { createCategory } from "./helper/adminapicall";
 
 const AddCategory = () => {
-  const [name, setName] = useState("initialState");
+  const [name, setName] = useState("");
   const [error, setError] = useState(false);
   const [success, setSuccess] = useState(false);
 
@@ -18,6 +19,39 @@ const AddCategory = () => {
     </div>
   );
 
+  const handleChange = (event) => {
+    setError("");
+    setName(event.target.value);
+  };
+
+  const onSubmit = (event) => {
+    event.preventDefault();
+    setError("");
+    setSuccess(false);
+
+    // backend request fired
+    createCategory(user._id, token, { name }).then((data) => {
+      if (data.error) {
+        setError(true);
+      } else {
+        setError("");
+        setSuccess(true);
+        setName("");
+      }
+    });
+  };
+
+  const successMessage = () => {
+    if (success) {
+      return <h4 className="text-success">Category created successfully</h4>;
+    }
+  };
+  const warningMessage = () => {
+    if (error) {
+      return <h4 className="text-success">Failed to create category</h4>;
+    }
+  };
+
   const myCategoryForm = () => (
     <form>
       <div className="form-group">
@@ -25,11 +59,15 @@ const AddCategory = () => {
         <input
           type="text"
           className="form-control my-3"
+          onChange={handleChange}
+          value={name}
           autoFocus
           required
           placeholder="For Ex. Summer"
         />
-        <button className="btn btn-outline-info">Create Category</button>
+        <button onClick={onSubmit} className="btn btn-outline-info">
+          Create Category
+        </button>
       </div>
     </form>
   );
@@ -42,6 +80,8 @@ const AddCategory = () => {
     >
       <div className="row bg-white rounded">
         <div className="col-md-8 offset-md-2">
+          {successMessage()}
+          {warningMessage()}
           {myCategoryForm()}
           {goBack()}
         </div>
