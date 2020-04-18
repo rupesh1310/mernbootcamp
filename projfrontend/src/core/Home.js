95181643088004fb00491cdc9f1beee4a1621035
rupesh1310 <a.rupesh66@gmail.com>
2020-04-19 04:42:09 +0530

    :art: :zap: creating a reusable card

--- projfrontend/src/core/Home.js
+++ projfrontend/src/core/Home.js
@@ -2,15 +2,14 @@ import React from "react";
 import "../styles.css";
 import { API } from "../backend";
 import Base from "./Base";
+import Card from "./Card";
 
 export default function Home() {
-  console.log("API IS", API);
-
   return (
     <Base title="Home Page" description="Welcome to the Tshirt Store">
-      <div className="row">
+      <div className="row text-center">
         <div className="col-4">
-          <button className="btn btn-success">TEST</button>
+          <Card />
         </div>
         <div className="col-4">
           <button className="btn btn-success">TEST</button>
