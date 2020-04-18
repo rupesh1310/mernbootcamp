5028c31539bdadda91f763864ff392698cd308f4
rupesh1310 <a.rupesh66@gmail.com>
2020-04-19 02:47:06 +0530

    :beetle: fixed categories bug

--- projfrontend/src/admin/AddProduct.js
+++ projfrontend/src/admin/AddProduct.js
@@ -2,8 +2,11 @@ import React, { useState, useEffect } from "react";
 import Base from "../core/Base";
 import { Link } from "react-router-dom";
 import { getCategories } from "./helper/adminapicall";
+import { isAutheticated } from "../auth/helper/index";
 
 const AddProduct = () => {
+  const { user, token } = isAutheticated();
+
   const [values, setValues] = useState({
     name: "",
     description: "",
@@ -35,7 +38,7 @@ const AddProduct = () => {
 
   const preload = () => {
     getCategories().then((data) => {
-      console.log(data);
+      //console.log(data);
       if (data.error) {
         setValues({ ...values, error: data.error });
       } else {
@@ -53,7 +56,9 @@ const AddProduct = () => {
   };
 
   const handleChange = (name) => (event) => {
-    //
+    const value = name === "photo" ? event.target.file[0] : event.target.value;
+    formData.set(name, value);
+    setValues({ ...values, [name]: value });
   };
 
   const createProductForm = () => (
@@ -104,8 +109,12 @@ const AddProduct = () => {
           placeholder="Category"
         >
           <option>Select</option>
-          <option value="a">a</option>
-          <option value="b">b</option>
+          {categories &&
+            categories.map((cate, index) => (
+              <option key={index} value={cate._id}>
+                {cate.name}
+              </option>
+            ))}
         </select>
       </div>
       <div className="form-group">
